76434c24b1f0157adda7e30ca8edb6936394f777
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-08 10:47:37 -0300

    create campo de erros

---
+++ ecommerce/src/app/shared/campo-control-error/campo-control-error.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campo-control-error',
+  templateUrl: './campo-control-error.component.html',
+  styleUrls: ['./campo-control-error.component.css']
+})
+export class CampoControlErrorComponent implements OnInit {
+
+  @Input() msgError!: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
